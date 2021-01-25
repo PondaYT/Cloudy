f6ea7f6b30a7b1dfeb1bf9cd8e63b870cee200ae
Joonwoo Kim <kjwamlex@gmail.com>
2021-01-25 02:28:04 -0500

    Bug fix on button with index 0 not changing its size

--- Cloudy/Fortnite/FortniteHUDCustomization.swift
+++ Cloudy/Fortnite/FortniteHUDCustomization.swift
@@ -18,13 +18,13 @@ class FortniteHUDCustomization: UIViewController {
     var HUDButtonHeight:[NSLayoutConstraint] = []
     var HUDButtonWidth:[NSLayoutConstraint] = []
     var buttonItems:[UIView] = []
-                               
+    
     var tagSelected = 256
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -38,7 +38,11 @@ class FortniteHUDCustomization: UIViewController {
             image.image = self.resizeImage(UIImage(named: "\(buttonImages).png")!, targetSize: CGSize(width: 100, height: 100))
             image.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
             button.frame = CGRect(x: x_axis, y: y_axis, width: 50, height: 50)
-            button.tag = buttonTag
+            if buttonTag == 0 {
+                button.tag = 300
+            } else {
+                button.tag = buttonTag
+            }
             button.addSubview(image)
             
             image.translatesAutoresizingMaskIntoConstraints = false
@@ -46,10 +50,8 @@ class FortniteHUDCustomization: UIViewController {
             let bottomConstraint = NSLayoutConstraint(item: image, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: button, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: 0)
             let leftConstraint = NSLayoutConstraint(item: image, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: button, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0)
             let rightConstraint = NSLayoutConstraint(item: image, attribute: NSLayoutConstraint.Attribute.right, relatedBy: NSLayoutConstraint.Relation.equal, toItem: button, attribute: NSLayoutConstraint.Attribute.right, multiplier: 1, constant: 0)
-            
             button.addConstraints([topConstraint, bottomConstraint, leftConstraint, rightConstraint])
             
-            print(button.tag)
             buttonItems.append(button)
             view.addSubview(button)
             x_axis += 50
@@ -67,7 +69,7 @@ class FortniteHUDCustomization: UIViewController {
     @objc func printSomething() {
         print("presed")
     }
-        
+    
     @IBAction func saveHUD() {
         HUDButtonRect.removeAll()
         for buttons in buttonItems {
@@ -84,7 +86,8 @@ class FortniteHUDCustomization: UIViewController {
     @IBAction func sliderValueChanged(sender: UISlider) {
         var currentValue = Int(sender.value)
         
-        if tagSelected < 255 {
+        if tagSelected <= 39 || tagSelected == 300 {
+            print("passed")
             let viewPassed = self.view.viewWithTag(self.tagSelected)
             viewPassed?.frame.size.height = (50*CGFloat(sender.value + 1.0))
             viewPassed?.frame.size.width = (50*CGFloat(sender.value + 1.0))
@@ -101,32 +104,30 @@ class FortniteHUDCustomization: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             var location = touch.location(in: self.view)
-            print(touch.view!.tag)
-            if touch.view!.tag <= 39 && touch.view! != slider {
+            if (touch.view!.tag <= 39 || touch.view!.tag == 300) && touch.view! != slider {
                 location.x -= touch.view!.frame.width/2
                 location.y -= touch.view!.frame.height/2
                 touch.view!.frame = CGRect.init(x: location.x, y: location.y, width: touch.view!.frame.width, height: touch.view!.frame.height)
-
-                } else {
-                    return
-                }
+                
+            } else {
+                return
             }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             var location = touch.location(in: self.view)
-            print(touch.view!.tag)
-            if touch.view!.tag <= 39  && touch.view! != slider {
+            if (touch.view!.tag <= 39 || touch.view!.tag == 300)  && touch.view! != slider {
                 location.x -= touch.view!.frame.width/2
                 location.y -= touch.view!.frame.height/2
                 touch.view!.frame = CGRect.init(x: location.x, y: location.y, width: touch.view!.frame.width, height: touch.view!.frame.height)
                 tagSelected = touch.view!.tag
-
-                } else {
-                    return
-                }
+                
+            } else {
+                return
             }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -136,10 +137,10 @@ class FortniteHUDCustomization: UIViewController {
     
     func resizeImage(_ image: UIImage, targetSize: CGSize) -> UIImage? {
         let size = image.size
-
+        
         let widthRatio  = targetSize.width  / image.size.width
         let heightRatio = targetSize.height / image.size.height
-
+        
         // Figure out what our orientation is, and use that to form the rectangle
         var newSize: CGSize
         if(widthRatio > heightRatio) {
@@ -147,16 +148,16 @@ class FortniteHUDCustomization: UIViewController {
         } else {
             newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
         }
-
+        
         // This is the rect that we've calculated out and this is what is actually used below
         let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-
+        
         // Actually do the resizing to the rect using the ImageContext stuff
         UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
         image.draw(in: rect)
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-
+        
         return newImage
     }
 }
