c6847f2b09abb04d4fbfd2c4365756937c726da0
exDeveloper <kjwamlex@gmail.com>
2021-03-06 13:43:53 -0500

    Added basic skeleton for Tutorial

---
+++ Cloudy/View/OscExtension/Fortnite/FortniteiPadTutorial.swift
@@ -0,0 +1,18 @@
+//
+//  FortniteiPadTutorial.swift
+//  Cloudy
+//
+//  Created by Joonwoo Kim on 2021-03-06.
+//  Copyright © 2021 Nomad5. All rights reserved.
+//
+
+import UIKit
+
+class FortniteiPadTutorial: UIViewController {
+    
+    var tutorialImageData:[UIImage] = []
+    var tutorialTitleData:[String] = []
+    var tutorialDescriptionData:[String] = []
+    
+    var dataIndex = 0
+}
---
+++ Cloudy/View/OscExtension/Fortnite/FortniteiPhoneTutorial.swift
@@ -0,0 +1,19 @@
+//
+//  FortniteiPhoneTutorial.swift
+//  Cloudy
+//
+//  Created by Joonwoo Kim on 2021-03-06.
+//  Copyright © 2021 Nomad5. All rights reserved.
+//
+
+import Foundation
+
+class FortniteiPhoneTutorial: UIViewController {
+    
+    var tutorialImageData:[UIImage] = []
+    var tutorialTitleData:[String] = []
+    var tutorialDescriptionData:[String] = []
+    
+    var dataIndex = 0
+    
+}
