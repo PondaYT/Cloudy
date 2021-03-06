23662bbdd91fafd9f66bdd0e0ce0ba0caaf716b5
exDeveloper <kjwamlex@gmail.com>
2021-03-06 13:51:51 -0500

    Basic functionalities for tutorial view controllers

--- Cloudy/View/OscExtension/Fortnite/FortniteiPhoneTutorial.swift
+++ Cloudy/View/OscExtension/Fortnite/FortniteiPhoneTutorial.swift
@@ -10,10 +10,28 @@ import Foundation
 
 class FortniteiPhoneTutorial: UIViewController {
     
+    @IBOutlet var tutorialImageView: UIImageView!
+    @IBOutlet var tutorialTitle: UILabel!
+    @IBOutlet var tutorialDescription: UITextView!
+    @IBOutlet var nextTutorialDataButton: UIButton!
+    @IBOutlet var previousTutorialDataButton: UIButton!
+    
     var tutorialImageData:[UIImage] = []
     var tutorialTitleData:[String] = []
     var tutorialDescriptionData:[String] = []
     
     var dataIndex = 0
+    
+    @IBAction func dismissTutorial() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func nextTutorialData() {
+        
+    }
+    
+    @IBAction func previousTutorialData() {
+        
+    }
     
 }
