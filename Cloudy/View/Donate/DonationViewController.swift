24f36f43035301bb3b8f6961559a1c5d65d8289f
Martin Mlostek <mlostek@gmail.com>
2021-03-10 19:37:09 +0100

    added donation reminder

---
+++ Cloudy/View/Donate/DonationViewController.swift
@@ -0,0 +1,43 @@
+// Copyright (c) 2021 Nomad5. All rights reserved.
+
+import Foundation
+import UIKit
+
+/// Specific reKairos donation reminder
+class DonationViewController: UIViewController {
+
+    /// Type for disappearing
+    typealias ViewControllerDismissed = () -> Void
+
+    /// Factory method
+    static func create(completion: @escaping ViewControllerDismissed) -> DonationViewController {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DonationViewController") as! DonationViewController
+        vc.completion = completion
+        return vc
+    }
+
+    /// Dismissed callback
+    var completion: ViewControllerDismissed? = nil
+
+    /// Gone
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        completion?()
+    }
+
+    /// Patreon pressed
+    @IBAction func onPatreonPressed(_ sender: Any) {
+        UIApplication.shared.open(Navigator.Config.Url.patreonReKairos)
+        UserDefaults.standard.didDonateAlready = true
+    }
+
+    /// Paypal pressed
+    @IBAction func onPayPalPressed(_ sender: Any) {
+        UIApplication.shared.open(Navigator.Config.Url.paypal)
+        UserDefaults.standard.didDonateAlready = true
+    }
+
+    @IBAction func onClosePressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
